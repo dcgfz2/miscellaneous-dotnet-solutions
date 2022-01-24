1a31051bcfaedaf7ddf389e0f4598ae41dee13b3
dcgfz2 <44886450+dcgfz2@users.noreply.github.com>
2022-01-24 04:58:07 +0000

    include Solutions namespace in wrapper csproj file

--- MainWrapper/Program.cs
+++ MainWrapper/Program.cs
@@ -1,1 +1,37 @@
-﻿// placeholder for now
+﻿using System;
+using Solutions;
+
+class Program{
+    static void Main(string[] args){
+        
+        Console.Clear();
+        Console.WriteLine("Choose an option:");
+        Console.WriteLine("1) Longest Common Prefix");
+
+        switch(Console.ReadLine()){
+            case "1":
+                Console.WriteLine(LongestCommonPrefix.Solve(TakeStringArray()));
+                break;
+            default:
+                Console.WriteLine("Not a valid option");
+                break;
+        }
+    }
+
+    private static string[] TakeStringArray(){
+        int size;
+        string[] array;
+
+        Console.WriteLine("How many words?:");//Limit 200
+        size = int.Parse(Console.ReadLine());
+        if(size > 200) size = 200;
+
+        Console.WriteLine("Enter words:");
+        array = new string[size];
+        
+        for(int i = 0; i < size; i++){
+          array[i] = Console.ReadLine();
+        }
+         return array;
+    }
+}
